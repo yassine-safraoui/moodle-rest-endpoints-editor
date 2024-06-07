fbcad2bb1826789ff83f7d47e34ba70b7e5ddce5
yassine safraoui <yassine.safraoui@grenoble-inp.org>
2024-06-07 17:14:06 +0200

    types file

---
+++ src/components/types.ts
@@ -0,0 +1,82 @@
+import z from "zod";
+
+export const EndpointParamsSchema = z.record(
+    z.string(),
+    z.object({
+        description: z.string().optional(),
+        type: z.string().optional().nullable(),
+        default: z.union([z.string(), z.number(), z.boolean(), z.array(z.string())])
+            .optional().nullable(),
+        required: z.boolean().optional(),
+        nullable: z.boolean().optional(),
+    }),
+);
+
+type IEndpointPrimitives = 'path' | 'area' | 'email' | 'tag' | 'int' | 'float' | 'safedir' | 'lang' | 'notags' | 'component' | 'raw' | 'localurl' | 'url' | 'alphanum' | 'bool' | 'alpha' | 'stringid' | 'file' | 'text' | 'theme' | 'raw_trimmed' | 'alphanumext' | 'plugin' | null;
+
+export type IEndpointResponse = {
+    description?: string;
+    nullable: boolean;
+} & (
+        {
+            type: "array";
+            items: IEndpointResponse;
+        } | {
+            type: "object";
+            properties: Record<string, IEndpointResponse>;
+        } | {
+            type: IEndpointPrimitives;
+        }
+    );
+
+// export const EndpointResponseSchema: z.ZodType<IEndpointResponse> = z.lazy(() =>
+//     z.object({
+//         description: z.string().optional(),
+//         nullable: z.boolean(),
+//     }).and(
+//         z.union([
+//             z.object({
+//                 type: z.literal("array"),
+//                 items: EndpointResponseSchema,
+//             }),
+//             z.object({
+//                 type: z.literal("object"),
+//                 properties: z.record(EndpointResponseSchema),
+//             }),
+//             z.object({
+//                 type: z.union(['path', 'area', 'email', 'tag', 'int', 'float', 'safedir', 'lang', 'notags', 'component', 'raw', 'localurl', 'url', 'alphanum', 'bool', 'alpha', 'stringid', 'file', 'text', 'theme', 'raw_trimmed', 'alphanumext', 'plugin'].map(s => z.literal(s))).nullable(),
+//             }),
+//         ]),
+//     ),
+// );
+
+// export function ValidateResponse(response: Record<string, any>): IEndpointResponse {
+//     if (!response['type']) throw new Error('Response type is required');
+//     if (response['type'] === 'array') {
+//         if (!response['items']) throw new Error('Response items is required for array type');
+//         return {
+//             description: response['description'],
+//             nullable: response['nullable'],
+//             type: 'array',
+//             items: ValidateResponse(response['items']),
+//         };
+//     }
+//     if (response['type'] === 'object') {
+//         if (!response['properties']) throw new Error('Response properties is required for object type');
+//         for (const key in response['properties']) {
+//             response['properties'][key] = ValidateResponse(response['properties'][key]);
+//         }
+//         return response as IEndpointResponse;
+//     }
+//     return response as IEndpointResponse;
+// }
+
+
+// type IEndpointResponsePrimitiveTypes = {'path', 'area', 'email', 'tag', 'int', 'float', None, 'safedir', 'lang', 'notags', 'component', 'raw', 'localurl', 'url', 'alphanum', 'bool', 'alpha', 'stringid', 'file', 'text', 'theme', 'raw_trimmed', 'alphanumext', 'plugin'}
+
+
+
+
+
+
+export type IEndpointParams = z.infer<typeof EndpointParamsSchema>;
