cda27bf7315e217ceb3ff9529667301aed3dd704
yassine safraoui <yassine.safraoui@grenoble-inp.org>
2024-06-08 21:15:52 +0200

    added sidebar tabs

--- src/app/(homepage)/layout.tsx
+++ src/app/(homepage)/layout.tsx
@@ -8,6 +8,10 @@ import { api } from "@/../convex/_generated/api";
 import { useState } from "react";
 import { usePathname } from "next/navigation";
 import Navbar from "@/components/Navbar";
+import { Tabs, TabsContent, TabsTrigger, TabsList } from "@/components/ui/tabs";
+import HiddenEndpointsList from "@/components/HiddenEndpointsList";
+import HiddenCategoriesList from "@/components/HiddenCategoriesList";
+import RelevantEndpointsList from "@/components/RelevantEndpointsList";
 
 export default function Dashboard({
   children,
@@ -26,7 +30,17 @@ export default function Dashboard({
   );
 
   return (
-    <div className="flex h-full w-full flex-row overflow-hidden">
+    <Tabs
+      orientation="vertical"
+      defaultValue="visible-endpoints"
+      className="flex h-full w-full flex-row gap-0 overflow-hidden p-0"
+    >
+      <TabsList className="m-0 justify-center rounded-none px-2">
+        <TabsTrigger value="relevant-endpoints">Relevant Endpoints</TabsTrigger>
+        <TabsTrigger value="visible-endpoints">Visible Endpoints</TabsTrigger>
+        <TabsTrigger value="hidden-categories">Hidden Categories</TabsTrigger>
+        <TabsTrigger value="hidden-endpoints">Hidden Endpoints</TabsTrigger>
+      </TabsList>
       <div className="flex h-full w-[36vw] flex-col border-r bg-muted/40 text-sm font-medium">
         <div className="relative my-4 w-full px-6">
           <Search className="absolute left-[2.1rem] top-2.5 h-4 w-4 text-muted-foreground" />
@@ -38,28 +52,93 @@ export default function Dashboard({
             onChange={(e) => setEndpointsFilter(e.target.value)}
           />
         </div>
-        <EndpointsList
-          filter={endpointsFilter}
-          endpointsList={endpointsList}
-          categoriesList={categoriesList}
-          categoriesLoading={categoriesLoading}
-          endpointsLoading={endpointsLoading}
-          activeCategory={
-            pathname.split("/").at(-2) == "category"
-              ? pathname.split("/").at(-1)
-              : undefined
-          }
-          activeEndpoint={
-            pathname.split("/").at(-2) == "endpoint"
-              ? pathname.split("/").at(-1)
-              : undefined
-          }
-        />
+        <TabsContent
+          value="relevant-endpoints"
+          className="m-0 h-full w-full overflow-hidden p-0"
+        >
+          <RelevantEndpointsList
+            filter={endpointsFilter}
+            endpointsList={endpointsList}
+            categoriesList={categoriesList}
+            categoriesLoading={categoriesLoading}
+            endpointsLoading={endpointsLoading}
+            activeCategory={
+              pathname.split("/").at(-2) == "category"
+                ? pathname.split("/").at(-1)
+                : undefined
+            }
+            activeEndpoint={
+              pathname.split("/").at(-2) == "endpoint"
+                ? pathname.split("/").at(-1)
+                : undefined
+            }
+          />
+        </TabsContent>
+        <TabsContent
+          value="visible-endpoints"
+          className="m-0 h-full w-full overflow-hidden p-0"
+        >
+          <EndpointsList
+            filter={endpointsFilter}
+            endpointsList={endpointsList}
+            categoriesList={categoriesList}
+            categoriesLoading={categoriesLoading}
+            endpointsLoading={endpointsLoading}
+            activeCategory={
+              pathname.split("/").at(-2) == "category"
+                ? pathname.split("/").at(-1)
+                : undefined
+            }
+            activeEndpoint={
+              pathname.split("/").at(-2) == "endpoint"
+                ? pathname.split("/").at(-1)
+                : undefined
+            }
+          />
+        </TabsContent>
+        <TabsContent
+          value="hidden-endpoints"
+          className="m-0 h-full w-full overflow-hidden p-0"
+        >
+          <HiddenEndpointsList
+            filter={endpointsFilter}
+            endpointsList={endpointsList}
+            categoriesList={categoriesList}
+            categoriesLoading={categoriesLoading}
+            endpointsLoading={endpointsLoading}
+            activeCategory={
+              pathname.split("/").at(-2) == "category"
+                ? pathname.split("/").at(-1)
+                : undefined
+            }
+            activeEndpoint={
+              pathname.split("/").at(-2) == "endpoint"
+                ? pathname.split("/").at(-1)
+                : undefined
+            }
+          />
+        </TabsContent>
+        <TabsContent
+          value="hidden-categories"
+          className="m-0 h-full w-full overflow-hidden p-0"
+        >
+          <HiddenCategoriesList
+            filter={endpointsFilter}
+            categoriesList={categoriesList}
+            categoriesLoading={categoriesLoading}
+            activeCategory={
+              pathname.split("/").at(-2) == "category"
+                ? pathname.split("/").at(-1)
+                : undefined
+            }
+          />
+        </TabsContent>
       </div>
+
       <div className="flex h-full w-full flex-col overflow-hidden">
         <Navbar />
         {children}
       </div>
-    </div>
+    </Tabs>
   );
 }
