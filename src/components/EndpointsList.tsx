656ef91a727cd23a8d0f1cf7252fa80c89df2704
yassine safraoui <yassine.safraoui@grenoble-inp.org>
2024-06-08 21:16:28 +0200

    added relevance icon switched to shadcn ScrollArea

--- src/components/EndpointsList.tsx
+++ src/components/EndpointsList.tsx
@@ -1,9 +1,10 @@
 import Link from "next/link";
 import { Doc } from "@/../convex/_generated/dataModel";
 import { useEffect, useRef } from "react";
-import { Eye, EyeOff } from "lucide-react";
+import { Eye, Star } from "lucide-react";
 import { useMutation } from "convex/react";
 import { api } from "../../convex/_generated/api";
+import { ScrollArea } from "./ui/scroll-area";
 
 export default function EndpointsList({
   filter,
@@ -30,6 +31,8 @@ export default function EndpointsList({
   const setEndpointVisibility = useMutation(
     api.endpoints.setEndpointVisibility,
   );
+  const setEndpointRelevance = useMutation(api.endpoints.setEndpointRelevance);
+  const setCategoryRelevance = useMutation(api.categories.setCategoryRelevance);
 
   useEffect(() => {
     if (activeCategoryElement.current) {
@@ -45,8 +48,6 @@ export default function EndpointsList({
         inline: "nearest",
       });
     }
-
-    return;
   }, [
     categoriesLoading,
     endpointsLoading,
@@ -62,100 +63,165 @@ export default function EndpointsList({
     );
   }
   return (
-    <ul className="flex max-h-full flex-col gap-1 overflow-hidden overflow-y-scroll pl-4">
-      {categoriesList.map((category) => (
-        <div
-          key={category._id}
-          hidden={
-            (!!filter &&
-              endpointsList.filter(
-                (endpoint) =>
-                  filter &&
-                  endpoint.categoryId === category._id &&
-                  endpoint.name.toLowerCase().includes(filter.toLowerCase()),
-              ).length === 0) ||
-            category.hidden
-          }
-        >
-          <div className="group flex w-full flex-row items-center gap-1">
-            <Link href={`/category/${category.name}`} legacyBehavior>
-              <a
-                className={`text-wrap px-2 italic hover:text-accent-foreground hover:underline ${
-                  category.name === activeCategory
-                    ? "rounded-md bg-slate-200 py-0.5 font-bold"
-                    : ""
-                }`}
-                ref={
-                  activeCategory === category.name
-                    ? activeCategoryElement
-                    : null
-                }
-              >
-                {category.name}
-              </a>
-            </Link>
-            <Eye
-              className="hidden cursor-pointer group-hover:inline-block"
-              size={"1rem"}
-              onClick={() =>
-                setCategoryVisibility({
-                  categoryId: category._id,
-                  hidden: true,
-                })
-              }
-            />
-          </div>
-          <ul className="flex flex-col gap-2 pl-6 pt-1">
-            {endpointsList
-              .filter((endpoint) => endpoint.categoryId === category._id)
-              .map((endpoint) => (
-                <div
-                  className="group flex w-full flex-row items-center gap-0.5 pr-4"
-                  key={endpoint._id}
+    <ScrollArea className="flex max-h-full flex-col pl-4 pt-1">
+      {categoriesList.map((category) => {
+        const categoryEndpoints = endpointsList.filter(
+          (endpoint) => endpoint.categoryId === category._id,
+        );
+        return (
+          <div
+            key={category._id}
+            hidden={
+              categoryEndpoints.filter((endpoint) => !endpoint.hidden)
+                .length === 0 ||
+              (!!filter &&
+                categoryEndpoints.filter(
+                  (endpoint) =>
+                    filter &&
+                    endpoint.name.toLowerCase().includes(filter.toLowerCase()),
+                ).length === 0) ||
+              category.hidden
+            }
+            className="mb-2"
+          >
+            <div className="group flex w-full flex-row items-center gap-1">
+              <Link href={`/category/${category.name}`} legacyBehavior>
+                <a
+                  className={`text-wrap px-2 italic hover:text-accent-foreground hover:underline ${
+                    category.name === activeCategory
+                      ? "rounded-md bg-slate-200 py-0.5 font-bold"
+                      : ""
+                  }`}
+                  ref={
+                    activeCategory === category.name
+                      ? activeCategoryElement
+                      : null
+                  }
                 >
-                  <Link
-                    legacyBehavior
-                    href={`/endpoint/${endpoint.name}`}
-                    hidden={
+                  {category.name}
+                </a>
+              </Link>
+              {categoryEndpoints.every((endpoint) => endpoint.relevant) ? (
+                <Star
+                  size={"1rem"}
+                  strokeWidth={0}
+                  fill="#ffe234"
+                  className="hidden cursor-pointer group-hover:inline-block"
+                  onClick={() =>
+                    setCategoryRelevance({
+                      categoryId: category._id,
+                      relevant: false,
+                    })
+                  }
+                />
+              ) : (
+                <Star
+                  className="hidden cursor-pointer group-hover:inline-block"
+                  size={"1rem"}
+                  onClick={() =>
+                    setCategoryRelevance({
+                      categoryId: category._id,
+                      relevant: true,
+                    })
+                  }
+                />
+              )}
+              {endpointsList.every((endpoint) => !endpoint.relevant) ? (
+                <Eye
+                  className="hidden cursor-pointer group-hover:inline-block"
+                  size={"1rem"}
+                  onClick={() =>
+                    setCategoryVisibility({
+                      categoryId: category._id,
+                      hidden: true,
+                    })
+                  }
+                />
+              ) : (
+                <Eye
+                  className="hidden group-hover:inline-block"
+                  color="muted"
+                  size={"1rem"}
+                />
+              )}
+            </div>
+            <ul className="flex flex-col gap-2 pl-6 pt-1">
+              {endpointsList
+                .filter((endpoint) => endpoint.categoryId === category._id)
+                .map((endpoint) => (
+                  <div
+                    className={`group w-full flex-row items-center gap-0.5 pr-4 ${
                       (!!filter &&
                         !endpoint.name
                           .toLowerCase()
                           .includes(filter.toLowerCase())) ||
                       endpoint.hidden
-                    }
+                        ? "hidden"
+                        : "flex"
+                    }`}
+                    key={endpoint._id}
                   >
-                    <a
-                      className={`block w-fit max-w-full overflow-hidden text-wrap px-2 hover:text-accent-foreground hover:underline ${
-                        endpoint.name === activeEndpoint
-                          ? "rounded-md bg-slate-200 py-0.5 font-bold"
-                          : ""
-                      }`}
-                      ref={
-                        activeEndpoint === endpoint.name
-                          ? activeEndpointElement
-                          : null
-                      }
-                    >
-                      {endpoint.name
-                        .split("_")
-                        .map((x, i) => (i ? [<wbr key={i} />, "_", x] : x))}
-                    </a>
-                  </Link>
-                  <Eye
-                    className="hidden cursor-pointer group-hover:inline-block"
-                    size={"1rem"}
-                    onClick={() =>
-                      setEndpointVisibility({
-                        endpointId: endpoint._id,
-                        hidden: true,
-                      })
-                    }
-                  />
-                </div>
-              ))}
-          </ul>
-        </div>
-      ))}
-    </ul>
+                    <Link legacyBehavior href={`/endpoint/${endpoint.name}`}>
+                      <a
+                        className={`block w-fit max-w-full overflow-hidden text-wrap px-2 hover:text-accent-foreground hover:underline ${
+                          endpoint.name === activeEndpoint
+                            ? "rounded-md bg-slate-200 py-0.5 font-bold"
+                            : ""
+                        }`}
+                        ref={
+                          activeEndpoint === endpoint.name
+                            ? activeEndpointElement
+                            : null
+                        }
+                      >
+                        {endpoint.name
+                          .split("_")
+                          .map((x, i) => (i ? [<wbr key={i} />, "_", x] : x))}
+                      </a>
+                    </Link>
+                    {endpoint.relevant ? (
+                      <Star
+                        size={"1rem"}
+                        strokeWidth={0}
+                        fill="#ffe234"
+                        className="cursor-pointer"
+                        onClick={() =>
+                          setEndpointRelevance({
+                            endpointId: endpoint._id,
+                            relevant: false,
+                          })
+                        }
+                      />
+                    ) : (
+                      <>
+                        <Star
+                          className="hidden cursor-pointer group-hover:inline-block"
+                          size={"1rem"}
+                          onClick={() =>
+                            setEndpointRelevance({
+                              endpointId: endpoint._id,
+                              relevant: true,
+                            })
+                          }
+                        />
+                        <Eye
+                          className="hidden cursor-pointer group-hover:inline-block"
+                          size={"1rem"}
+                          onClick={() =>
+                            setEndpointVisibility({
+                              endpointId: endpoint._id,
+                              hidden: true,
+                            })
+                          }
+                        />
+                      </>
+                    )}
+                  </div>
+                ))}
+            </ul>
+          </div>
+        );
+      })}
+    </ScrollArea>
   );
 }
