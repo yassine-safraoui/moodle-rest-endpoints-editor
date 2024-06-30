cec04a99ee3bd5b488ce2042873002c3c5b61f7d
yassine safraoui <yassine.safraoui@grenoble-inp.fr>
2024-06-30 20:15:51 +0200

    endpoints sidebar is now collapsible

--- src/app/(homepage)/layout.tsx
+++ src/app/(homepage)/layout.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Search } from "lucide-react";
+import { ChevronsLeft, ChevronsRight, Search } from "lucide-react";
 
 import { Input } from "@/components/ui/input";
 import EndpointsList from "@/components/EndpointsList";
@@ -32,8 +32,12 @@ export default function Dashboard({
   const searchParams = useSearchParams();
 
   const createQueryString = useCallback(
-    (name: string, value: string) => {
+    (name: string, value: string | undefined) => {
       const params = new URLSearchParams(searchParams.toString());
+      if (value === undefined) {
+        params.delete(name);
+        return params.toString();
+      }
       params.set(name, value);
       return params.toString();
     },
@@ -75,70 +79,106 @@ export default function Dashboard({
         <TabsTrigger value="hidden-categories">Hidden Categories</TabsTrigger>
         <TabsTrigger value="hidden-endpoints">Hidden Endpoints</TabsTrigger>
       </TabsList>
-      <div className="flex h-full w-[36vw] flex-col border-r bg-muted/40 text-sm font-medium">
-        <div className="relative my-4 w-full px-6">
-          <Search className="absolute left-[2.1rem] top-2.5 h-4 w-4 text-muted-foreground" />
-          <Input
-            type="search"
-            placeholder="Search endpoints..."
-            className="w-full appearance-none bg-background pl-8 shadow-none"
-            value={endpointsFilter}
-            onChange={(e) => setEndpointsFilter(e.target.value)}
-          />
+      <div
+        className={`${
+          searchParams.get("collapse") === "true" ? "w-0" : "w-[25vw]"
+        } h-full max-w-[25vw] flex-shrink-0 overflow-hidden`}
+        style={{ transition: "width", transitionDuration: "0.3s" }}
+      >
+        <div className="flex h-full w-[25vw] flex-col border-r bg-muted/40 text-sm font-medium">
+          <div className="flex w-full flex-row place-content-center gap-2">
+            <div className="relative my-4 w-full pl-6">
+              <Search className="absolute left-[2.1rem] top-2.5 h-4 w-4 text-muted-foreground" />
+              <Input
+                type="search"
+                placeholder="Search endpoints..."
+                className="w-full appearance-none bg-background pl-8 shadow-none"
+                value={endpointsFilter}
+                onChange={(e) => setEndpointsFilter(e.target.value)}
+              />
+            </div>
+            <ChevronsLeft
+              className="h-full cursor-pointer"
+              size={"35"}
+              onClick={() =>
+                router.push(
+                  pathname + "?" + createQueryString("collapse", "true"),
+                )
+              }
+            />
+          </div>
+          <TabsContent
+            value="relevant-endpoints"
+            className="m-0 h-full w-full overflow-hidden p-0"
+          >
+            <RelevantEndpointsList
+              filter={endpointsFilter}
+              endpointsList={endpointsList}
+              categoriesList={categoriesList}
+              categoriesLoading={categoriesLoading}
+              endpointsLoading={endpointsLoading}
+              activeCategory={activeCategory}
+              activeEndpoint={activeEndpoint}
+            />
+          </TabsContent>
+          <TabsContent
+            value="visible-endpoints"
+            className="m-0 h-full w-full overflow-hidden p-0"
+          >
+            <EndpointsList
+              filter={endpointsFilter}
+              endpointsList={endpointsList}
+              categoriesList={categoriesList}
+              categoriesLoading={categoriesLoading}
+              endpointsLoading={endpointsLoading}
+              activeCategory={activeCategory}
+              activeEndpoint={activeEndpoint}
+            />
+          </TabsContent>
+          <TabsContent
+            value="hidden-endpoints"
+            className="m-0 h-full w-full overflow-hidden p-0"
+          >
+            <HiddenEndpointsList
+              filter={endpointsFilter}
+              endpointsList={endpointsList}
+              categoriesList={categoriesList}
+              categoriesLoading={categoriesLoading}
+              endpointsLoading={endpointsLoading}
+              activeCategory={activeCategory}
+              activeEndpoint={activeEndpoint}
+            />
+          </TabsContent>
+          <TabsContent
+            value="hidden-categories"
+            className="m-0 h-full w-full overflow-hidden p-0"
+          >
+            <HiddenCategoriesList
+              filter={endpointsFilter}
+              categoriesList={categoriesList}
+              categoriesLoading={categoriesLoading}
+              activeCategory={activeCategory}
+            />
+          </TabsContent>
         </div>
-        <TabsContent
-          value="relevant-endpoints"
-          className="m-0 h-full w-full overflow-hidden p-0"
-        >
-          <RelevantEndpointsList
-            filter={endpointsFilter}
-            endpointsList={endpointsList}
-            categoriesList={categoriesList}
-            categoriesLoading={categoriesLoading}
-            endpointsLoading={endpointsLoading}
-            activeCategory={activeCategory}
-            activeEndpoint={activeEndpoint}
-          />
-        </TabsContent>
-        <TabsContent
-          value="visible-endpoints"
-          className="m-0 h-full w-full overflow-hidden p-0"
-        >
-          <EndpointsList
-            filter={endpointsFilter}
-            endpointsList={endpointsList}
-            categoriesList={categoriesList}
-            categoriesLoading={categoriesLoading}
-            endpointsLoading={endpointsLoading}
-            activeCategory={activeCategory}
-            activeEndpoint={activeEndpoint}
-          />
-        </TabsContent>
-        <TabsContent
-          value="hidden-endpoints"
-          className="m-0 h-full w-full overflow-hidden p-0"
-        >
-          <HiddenEndpointsList
-            filter={endpointsFilter}
-            endpointsList={endpointsList}
-            categoriesList={categoriesList}
-            categoriesLoading={categoriesLoading}
-            endpointsLoading={endpointsLoading}
-            activeCategory={activeCategory}
-            activeEndpoint={activeEndpoint}
-          />
-        </TabsContent>
-        <TabsContent
-          value="hidden-categories"
-          className="m-0 h-full w-full overflow-hidden p-0"
-        >
-          <HiddenCategoriesList
-            filter={endpointsFilter}
-            categoriesList={categoriesList}
-            categoriesLoading={categoriesLoading}
-            activeCategory={activeCategory}
-          />
-        </TabsContent>
+      </div>
+      <div
+        className="relative mt-14 h-full w-0"
+        style={{
+          opacity: searchParams.get("collapse") === "true" ? 1 : 0,
+          transition: "opacity",
+          transitionDuration: "0.3s",
+        }}
+      >
+        <ChevronsRight
+          className="absolute left-0 top-4 cursor-pointer"
+          size={"35"}
+          onClick={() =>
+            router.push(
+              pathname + "?" + createQueryString("collapse", undefined),
+            )
+          }
+        />
       </div>
 
       <div className="flex h-full w-full flex-col overflow-hidden">
